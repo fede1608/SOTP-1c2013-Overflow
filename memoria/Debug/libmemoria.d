c42d501f4055db84816a9c54c5f7cc6484478b93
Federico Casares <overflowDT@overflow1021>
2013-05-21 20:29:39 -0300

    Agregado de: sacar plan en el nivel del pj, movimiento hacia los recursos de los pjs y recorrido secuencial de los recursos a buscar.

--- memoria/Debug/libmemoria.d
+++
@@ -1,8 +0,0 @@
-libmemoria.d: ../libmemoria.c ../libmemoria.h \
- ../commons/collections/list.h ../commons/collections/node.h
-
-../libmemoria.h:
-
-../commons/collections/list.h:
-
-../commons/collections/node.h:
