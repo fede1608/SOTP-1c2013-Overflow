6688268ced818db9b057ba77687e03c935e8df65
fedecasares <fedecasares1993@yahoo.com.ar>
2013-07-02 18:46:16 -0300

    Organizacion del codigo en nivel y personaje

--- ProcesoNivel/nivel.c
+++ ProcesoNivel/nivel.c
@@ -72,7 +72,29 @@ void sacarInfoCaja(char * caja, char* id, int* x , int* y, int* cant);
 //thread adicional que cada cierto tiempo (configurable)
 //chequee que un personaje este bloqueado (lo veremos despues esto)
 int main(void){
-
+	//	Leer el config y cargar los recursos
+	//	CrearCaja(&listaItems,'F',10,14,7);
+	//	CrearCaja(&listaItems,'H',17,6,3);
+	t_config* configNivel = config_create("config.txt");
+	char *varstr;
+	varstr=malloc(8);
+	strcpy(varstr,"Caja");
+	char pal[4];
+	int cantKeys = config_keys_amount(configNivel)-4;
+	int x;
+	for (x=1;x<=cantKeys;x++)
+		{
+			sprintf(pal, "%d", x);
+			strcpy(varstr+4,pal);
+			if(config_has_property(configNivel,varstr))
+			{
+				char *stringCompleto = config_get_string_value(configNivel,varstr);
+				char id;
+				int posx, posy, instancias;
+				sacarInfoCaja(stringCompleto, &id, &posx, &posy, &instancias);
+				CrearCaja(&listaItems, id, posx, posy, instancias);
+			}
+		}
 
 	//Crea archivo log
 	//Si existe lo abre, sino, lo crea
@@ -97,33 +119,7 @@ int main(void){
 
 
 
-		//	Leer el config y cargar los recursos
-//		CrearCaja(&listaItems,'F',10,14,7);
-//		CrearCaja(&listaItems,'H',17,6,3);
-		t_config* configNivel = config_create("config.txt");
-			char *varstr;
-			varstr=malloc(8);
-			strcpy(varstr,"Caja");
-			char pal[4];
-			int cantKeys = config_keys_amount(configNivel)-4;
-			int x;
-			for (x=1;x<=cantKeys;x++)
-			{
 
-					sprintf(pal, "%d", x);
-					strcpy(varstr+4,pal);
-
-					if(config_has_property(configNivel,varstr))
-						{
-						char *stringCompleto = config_get_string_value(configNivel,varstr);
-
-						char id;
-						int posx, posy, instancias;
-
-						sacarInfoCaja(stringCompleto, &id, &posx, &posy, &instancias);
-						CrearCaja(&listaItems, id, posx, posy, instancias);
-						}
-				}
 
 		while(1){
 
--- ProcesoPersonaje/personaje.c
+++ ProcesoPersonaje/personaje.c
@@ -29,12 +29,13 @@ int lengthVecConfig(char * value);
 int main(void){
 	t_config* config=config_create("config.txt");
 	//obtener recurso de config
-			char** obj;
+			char** obj,niveles;
 			char* val;
+			char objNivel[14];
 			int veclong;
 			int llego;
 			Header h;
-
+			niveles=config_get_array_value(config,"planDeNiveles");
 			obj=config_get_array_value(config,"obj[Nivel1]");
 			val=config_get_string_value(config,"obj[Nivel1]");
 			charPer=config_get_string_value(config,"simbolo")[0];
@@ -44,34 +45,23 @@ int main(void){
 		//TODO implementar for de niveles, conexion solicitud de ip:puerto al orquestator y cierre de esa conex
 
 			//conectar con nivel, y planificador
-
 			int unSocket;
-
 			unSocket = quieroUnPutoSocketAndando("127.0.0.1",5000);
 
-
-
+			//esperar Primer movPermitido
 			char* charbuf;
 			charbuf=malloc(1);
 			*charbuf=charPer;
 
 			if (mandarMensaje(unSocket,0 , 1,charbuf)) {
-
 				printf("Llego el OK al nivel\n");
-
 				if(recibirMensaje(unSocket, (void**)&charbuf)>=0) {
-
 					printf("Llego el OK del nivel char %c\n",*charbuf);
-
 				}
-
 				else
 					printf("No llego al cliente la confirmacion del nivel (handshake)\n");
-
 			} else {
-
 				printf("No llego al nivel la confirmacion del personaje (handshake)\n");
-
 			}
 
 int ii;
@@ -103,27 +93,44 @@ for(ii=0;ii<veclong;ii++){
 	//solicitar posicion recurso Recurso: recactual
 	//esperar posicion del recurso posicion : rec
 	llego=1;
+
 	while(llego){
-				//TODO esperar mensaje de movPermitido para continuar
+
+		//TODO esperar mensaje de movPermitido para continuar
 		printf("pos %d %d \n",pos.x,pos.y);
 		if (pos.x!=rec.x) {
 			(rec.x-pos.x)>0?pos.x++:pos.x--;
 		}
 		else if (pos.y!=rec.y) {
 			(rec.y-pos.y)>0?pos.y++:pos.y--;
-		} else {
-					//Solicitar instancia del recActual
-					//esperar OK
+		}
+
+		//aviso posicion pos al nivel
+		h.type = 2;
+		buffer=&pos;
+		char* sth;
+
+		if (mandarMensaje(unSocket,2 , sizeof(Posicion),buffer)) {
+			printf("Llego el header de la posicion del personaje %d %d\n",pos.x,pos.y);
+			if (recibirMensaje(unSocket, (void**)&sth)>=0) {
+				printf("Llego header respuesta: %c del nivel\n",*sth);
+			}
+		}
+
+		if((pos.y==rec.y)&(pos.x==rec.x)) {
+			//Solicitar instancia del recActual
+			//esperar OK
 			h.type = 3;
 			h.payloadlength = 1;
 			llego=0;
-//			recActual=*obj[ii+1];
 			buffer = &recActual;
+
 			if (mandarMensaje(unSocket,(int8_t)3 , sizeof(char),buffer)) {
 				printf("Llego el header de peticion de recurso %c al nivel\n",recActual);
 				printf("Llego el buffer del recurso necesario al nivel\n");
 				Header unHeader;
 				Posicion lifeSucks;
+
 				if (recibirHeader(unSocket,&unHeader)) {
 					printf("Llego header%d %d respuesta del nivel\n", unHeader.payloadlength,unHeader.type);
 					recibirData(unSocket,unHeader,(void**)&lifeSucks);
@@ -133,15 +140,18 @@ for(ii=0;ii<veclong;ii++){
 			}
 
 			if(ii+1==veclong) {
+				//cerrar conexion con el nivel
 				mandarMensaje(unSocket,4 , sizeof(char),&recActual);
-				exit(0); //todo settear variable de escape de nivel terminado
+				//exit(0); //todo settear variable de escape de nivel terminado
 			}else {
 				recActual=*obj[ii+1];
 				buffer=&recActual;
+
 				if (mandarMensaje(unSocket,1, sizeof(char),buffer)) {
 					printf("Llego el header de la posicion a recurso al nivel\n");
 					printf("Llego el recurso actual necesario al nivel\n");
 					Header unHeader;
+
 					if (recibirHeader(unSocket,&unHeader)) {
 						printf("pos %d %d %d %d\n",pos.x,pos.y,unHeader.payloadlength,unHeader.type);
 						Posicion lifeSucks;
@@ -150,20 +160,14 @@ for(ii=0;ii<veclong;ii++){
 						printf("Llego %c header respuesta del nivel: %d %d\n",recActual,rec.x,rec.y);
 						printf("Llego la posicion del recurso solicitado del nivel\n");
 					}
+
 				}
+
 			}
+
 		}
-				//aviso posicion pos al nivel
-		h.type = 2;
-		buffer=&pos;
-		char* sth;
-		if (mandarMensaje(unSocket,2 , sizeof(Posicion),buffer)) {
-			printf("Llego el header de la posicion del personaje %d %d\n",pos.x,pos.y);
-			if (recibirMensaje(unSocket, (void**)&sth)>=0) {
-				printf("Llego header respuesta: %c del nivel\n",*sth);
-			}
-		}
-		sleep(1);
+		//esperar MovPermitido
+		sleep(1);//todo sacar
 	}//fin while(llego)
 }//fin for
 
